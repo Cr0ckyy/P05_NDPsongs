ec1ba1ff4a3784efe5a99cc6e1f3d2c32638dd81
Myron819 <19036849@rp.edu.sg>
2021-05-06 16:07:16 +0000

    Updated

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -2,10 +2,12 @@ package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,19 +21,12 @@ public class MainActivity extends AppCompatActivity {
         Button btnInsert;
         Button btnShowList;
         final EditText etNote;
-        final RadioButton rb1;
-        final RadioButton rb2;
-        final RadioButton rb3;
-        final RadioButton rb4;
-        final RadioButton rb5;
+        final RadioGroup rg;
 
         btnInsert = findViewById(R.id.buttonInsertNote);
         btnShowList = findViewById(R.id.buttonShowList);
         etNote = findViewById(R.id.editTextNote);
-        rb1 = findViewById(R.id.radio1);
-        rb2 = findViewById(R.id.radio2);
-        rb3 = findViewById(R.id.radio3);
-        rb4 = findViewById(R.id.radio4);
+        rg = findViewById(R.id.radioGroupStars);
 //        rb5 = findViewById(R.id.radio5);
 
         btnInsert.setOnClickListener(new View.OnClickListener(){
@@ -41,21 +36,28 @@ public class MainActivity extends AppCompatActivity {
                 // activity's Context
                 DBHelper db = new DBHelper(MainActivity.this);
 
-                int stars;
-                if (rb1.isSelected()) {
+                int stars = 0;
+                if ((rg.getCheckedRadioButtonId()) == 2131165316) {
                     stars = 1;
-                } else if (rb2.isSelected()) {
+                } else if ((rg.getCheckedRadioButtonId()) == 2131165317) {
                     stars = 2;
-                } else if (rb3.isSelected()) {
+
+                } else if ((rg.getCheckedRadioButtonId()) == 2131165318) {
                     stars = 3;
-                } else if (rb4.isSelected()) {
+
+                } else if ((rg.getCheckedRadioButtonId()) == 2131165319) {
                     stars = 4;
+
                 } else {
                     stars = 5;
+
                 }
+                Log.i("Myron", rg.getCheckedRadioButtonId() + "");
 
                 // Insert a task
-                db.insertNote(etNote.getText().toString(), stars);
+//                Log.i("Myron", stars + "");
+
+                db.insertNote("Myron", stars);
                 db.close();
             }
         });

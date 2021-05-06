06d09a12454c6decd540069a117943aed2d02e74
Myron819 <19036849@rp.edu.sg>
2021-05-06 14:32:38 +0000

    Removed unnecessary setters

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,14 +1,30 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+// TODO: Done by Shufang
+// TODO: Create the necessary objects in the SecondActivity to bind the ListView components.
 public class SecondActivity extends AppCompatActivity {
+
+	RevisionNotesArrayAdapter revisionNotesArrayAdapter;
+	ArrayList<Note> noteArrayList;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		//TODO implement the Custom ListView
+
+
+
+
+
 
 	}
 
